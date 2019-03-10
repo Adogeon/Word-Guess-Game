7f38bb233879e1122e53facbd2c986293e18ca4c
Anh Dao <addao823@gmail.com>
2019-03-09 20:05:25 -0800

    Add function displayWord() & displayBox()

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,4 +1,3 @@
-console.log("Hello World!");
 
 class wordAnswer {
     constructor (question,text) {
@@ -32,3 +31,57 @@ class wordAnswer {
     } 
 }
 
+//trying to parse the text file into the script and put it into an array
+/*function readTextFile(file) {
+    var rawInput = new XMLHttpRequest();
+    rawInput.open("GET", file, false);
+    rawInput.onreadystatechange = function (){
+        if(rawInput.readyState === 4) {
+            if(rawInput.status === 200 || rawInput.status === 0) {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}*/
+//untested work. Return an array of Word Object from file.
+
+//declaration for test purpose
+var test1 = new wordAnswer("What is the name of Hoenn water starter ?","Mudkip");
+var test2 = new wordAnswer("What is the name of Kanto leaf starter ?","Bulbasaur");
+
+var tests = [test1,test2];
+
+console.log(test1);
+console.log(test2);
+
+//function to dynamically display the question on board
+function displayWord(wordAnswerInst) {
+    var questionEl = document.getElementById("question");
+    questionEl.textContent = wordAnswerInst.question;
+    console.log("hello");
+}
+
+//function to add character box on board
+function displayBox(wordAnswerInst) {
+    var answer = wordAnswerInst.text.split('');
+    console.log(answer);
+    for (i=0;i<answer.length;i++) {
+        var boxSpan = document.createElement('span');
+        var textInBox = document.createTextNode(answer[i].toUpperCase());
+        boxSpan.appendChild(textInBox);
+        boxSpan.className = "h1";
+        var boxWrap = document.createElement('div');
+        boxWrap.setAttribute('style','width:70px;height:70px');
+        boxWrap.className = "border bd-dark bg-white mx-3 d-flex justify-content-center align-middle";
+        boxWrap.appendChild(boxSpan);
+        document.getElementById("answer-display").appendChild(boxWrap);
+    }
+    console.log("hi")
+}
+
+
+
+displayWord(test1);
+displayBox(test1);
