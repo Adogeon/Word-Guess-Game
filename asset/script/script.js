4e4578cc6d594cefcdf1c9f1666fb6cd25b81248
Anh Dao <addao823@gmail.com>
2019-03-09 22:13:26 -0800

    Finished, haven't debug

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,4 +1,3 @@
-
 class wordAnswer {
     constructor (question,text) {
         this.question = question;
@@ -11,7 +10,7 @@ class wordAnswer {
         var result = [];
         (this.text).split("").forEach(function(element){
             if(!(result.includes(element))) {
-                result.push(element);
+                result.push(element.toLowerCase());
             }
         })
         return result;
@@ -48,13 +47,49 @@ class wordAnswer {
 //untested work. Return an array of Word Object from file.
 
 //declaration for test purpose
-var test1 = new wordAnswer("What is the name of Hoenn water starter ?","Mudkip");
-var test2 = new wordAnswer("What is the name of Kanto leaf starter ?","Bulbasaur");
+var word1 = new wordAnswer("Kanto leaf-type starter","bulbasaur");
+var word2 = new wordAnswer("Kanto fire-type starter","charmander");
+var word3 = new wordAnswer("Kanto water-type starter","squirtle");
+var word4 = new wordAnswer("Johto leaf-type starter","chikorita");
+var word5 = new wordAnswer("Johto fire-type starter","totodile");
+var word6 = new wordAnswer("Johto water-type starter","cyndaquil");
+var word7 = new wordAnswer("Hoenn leaf-type starter","treeko");
+var word8 = new wordAnswer("Hoenn fire-type starter","torchic");
+var word9 = new wordAnswer("Hoenn water-type starter","mudkip");
+var words = [word1,word2,word3,word4,word5,word6,word7,word8,word9];
 
-var tests = [test1,test2];
+var randomNumber = Math.floor(Math.random()*words.length);
+var currentWord = words[randomNumber];
+var fail = 5;
+var state = 0;
+//var score = currentWord.
 
-console.log(test1);
-console.log(test2);
+//display score
+function displayFail() {
+    if (fail > 1) {
+        document.getElementById('score').textContent = fail;
+    } else if (fail === 1) {
+        var scoreBoardEle = document.getElementById('score');
+        while(scoreBoardEle.firstChild) {
+            scoreBoardEle.removeChild(scoreBoardEle.firstChild);
+        }
+        var warnEle = document.createElement('span')
+        var warnText = document.createTextNode("Be carefule! You only have 1 try left")
+        warnEle.appendChild(warnText)
+        scoreBoardEle.appendChild(warnEle);
+    } else if (fail === 0) {
+        state = 1;
+        var scoreBoardEle = document.getElementById('score');
+        while(scoreBoardEle.firstChild) {
+            scoreBoardEle.removeChild(scoreBoardEle.firstChild);
+        }
+        var warnEle = document.createElement('span')
+        var warnText = document.createTextNode("Be carefule! You only have 1 try left")
+        warnEle.appendChild(warnText)
+        scoreBoardEle.appendChild(warnEle);
+    }
+    
+}
 
 //function to dynamically display the question on board
 function displayWord(wordAnswerInst) {
@@ -69,19 +104,93 @@ function displayBox(wordAnswerInst) {
     console.log(answer);
     for (i=0;i<answer.length;i++) {
         var boxSpan = document.createElement('span');
-        var textInBox = document.createTextNode(answer[i].toUpperCase());
+        var textInBox = document.createTextNode("_");
         boxSpan.appendChild(textInBox);
         boxSpan.className = "h1";
+        boxSpan.setAttribute("id","box-text-"+i)
         var boxWrap = document.createElement('div');
-        boxWrap.setAttribute('style','width:70px;height:70px');
-        boxWrap.className = "border bd-dark bg-white mx-3 d-flex justify-content-center align-middle";
+        boxWrap.style.height="70px";
+        boxWrap.style.width="70px";
+        boxWrap.className = "border bd-dark bg-white mx-3 d-flex justify-content-center align-middle ";
         boxWrap.appendChild(boxSpan);
         document.getElementById("answer-display").appendChild(boxWrap);
     }
-    console.log("hi")
 }
 
+displayWord(currentWord);
+displayBox(currentWord);
+displayFail();
+
+//keyEventListener to check for the answer 
+document.addEventListener("keyup", function(event) {
+    //check if state is playing (0) 
+    if (state !== 0) {
+        return;
+    }
+    //check if key press is correct
+    var keyAnswer = event.key;
+    var oldAnswer = [];
+    if((currentWord.answerKey.includes(keyAnswer)) && (!oldAnswer.includes(keyAnswer))) {
+        var answerIndex = currentWord.getAnswerIndex(keyAnswer);
+        oldAnswer.push(keyAnswer);
+        answerIndex.forEach(function(Element) {
+            console.log(Element);
+            var currentSpan = document.getElementById("box-text-"+Element);
+            currentSpan.textContent = keyAnswer.toUpperCase();
+        })
+        //in case of won
+        if (oldAnswer.length === currentWord.answerKey.length) {
+            state = 1;
+            var gameConsoleEle = document.getElementById('game-text');
+            while(gameConsoleEle.firstChild) {
+                gameBoardEle.removeChild(gameConsoleEle.firstChild);
+            }
+            var winEle = document.createElement('span')
+            var winText = document.createTextNode("Congrats, you have win");
+            winEle.appendChild(winText)
+            gameConsoleEle.appendChild(winEle)
+        }
+    } else if (oldAnswer.includes(keyAnswer)) {
+        // guess correct again, don't do anything
+        return;
+    } else {
+        //fail answer
+        score--;
+        var wrongGuessList = document.getElementById('wrong-guess');
+        var wrongGuessSpan = document.createElement('span');
+        var wrongGuessText = document.createTextNode(keyAnswer.toUpperCase());
+        wrongGuessSpan.appendChild(wrongGuessText);
+        wrongGuessSpan.className = "mx-1";
+        wrongGuessList.appendChild(wrongGuessSpan);
+        displayFail()
+    }
+})
+
+var resetButton = document.getElementById("reset-button")
+resetButton.addEventListener ("click", function(){
+    randomNumber = Math.floor(Math.random()*words.length);
+    currentWord = word(randomNumber)
+    displayWord(currentWord)
+    displayBox(curentWord)
+    fail = 5;
+    state = 0;
+    var scoreBoardEle = document.getElementById('score');
+    while(scoreBoardEle.firstChild) {
+        scoreBoardEle.removeChild(scoreBoardEle.firstChild);
+    }
+    var scoreEle = document.createElement('span')
+    var scoreText = document.createTextNode("You have")
+    scoreEle.appendChild(scoreText)
+    scoreBoardEle.appendChild(scoreEle);
+    scoreEle = document.createElement('span');
+    scoreEle.setAttribute('id','score');
+    scoreBoardEle.appendChild(scoreEle);
+    scoreEle = document.createElement('span');
+    scoreText = document.createTextNode(" tries.");
+    scoreEle.appendChild(scoreText)
+    scoreBoardEle.appendChild(scoreEle);
+})
 
 
-displayWord(test1);
-displayBox(test1);
+
+
